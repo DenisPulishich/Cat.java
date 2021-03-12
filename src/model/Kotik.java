124cee00896dc727dd01696c15c80dd8441fdbe4
patro <patrol2100@yandex.ru>
2021-03-12 18:25:57 +0300

    done2

--- src/model/Kotik.java
+++ src/model/Kotik.java
@@ -1,9 +1,7 @@
 package model;
 
-import java.util.Random;
-public class Kotik
 
-{
+public class Kotik {
     private Double originWeight;
     private Double weight;
     private String name;
@@ -11,22 +9,23 @@ public class Kotik
     //сытость котика
     private double catSatiety;
 
-    private final Double MIN_WEIGHT = 1000.0;;
-    private final Double MAX_WEIGHT = 9000.0;;
+    private final Double MIN_WEIGHT = 1000.0;
+    ;
+    private final Double MAX_WEIGHT = 9000.0;
+    ;
     // Счет созданных котов
     private static int count;
 
 
-
-    public Kotik()
-    {
+    public Kotik() {
         count++;
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         catSatiety = 10.0 + 20.0 * Math.random();
 
     }
-    public Kotik (String name, double weight, double catSatiety, String Meow){
+
+    public Kotik(String name, Double weight, Double catSatiety, String Meow) {
         count++;
         this.name = name;
         this.weight = weight;
@@ -34,41 +33,42 @@ public class Kotik
         this.Meow = Meow;
     }
 
-        public void setKotik (int prettiness, String name, double weight, String Meow){
+    public void setKotik(int prettiness, String name, Double weight, String Meow) {
         this.weight = weight;
         this.Meow = Meow;
-        }
+    }
 
 
-
-
-    public double getCatSatiety(){
+    public double getCatSatiety() {
         return catSatiety;
     }
 
-    public boolean meow()
-    {   if (catSatiety>0){
-        weight = weight - 1;
-        System.out.println("Meow");
-        return true;}
-        else {
-        System.out.println("Сначала покорми");
-        return false;
+    public boolean meow() {
+        if (catSatiety > 0) {
+            weight = weight - 1;
+            System.out.println("Meow");
+            return true;
+        } else {
+            System.out.println("Сначала покорми");
+            return false;
+        }
     }
-    }
-        // Три метода eat
-    public void eat(Double amount)
-    {
+
+    // Три метода eat
+    public void eat(Double amount) {
         catSatiety = catSatiety + amount;
         System.out.println("Cat eating");
     }
-    public void eat(Double amount, String foodName){
+
+    public void eat(Double amount, String foodName) {
         catSatiety = catSatiety + amount;
         System.out.println("Cat eating " + foodName);
     }
-    public void eat(){
+
+    public void eat() {
         eat();
     }
+
     public boolean drink(Double amount) {
         if (catSatiety > 0) {
             weight = weight + amount;
@@ -80,81 +80,89 @@ public class Kotik
         }
     }
 
-    public boolean play()
-    {
-        if (catSatiety>0){
+    public boolean play() {
+        if (catSatiety > 0) {
             System.out.println("Cat playing");
             return true;
-        }
-        else {
+        } else {
             System.out.println("Сначла покорми");
             return false;
         }
     }
 
 
-
-    public Double getWeight()
-    {
+    public Double getWeight() {
         return weight;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public String getMeow(){
+
+    public String getMeow() {
         return Meow;
     }
 
 
-    public double amountOfFoodEaten (){
+    public double amountOfFoodEaten() {
         double result = weight - originWeight;
         return result;
     }
-    public boolean pee (){
-        if(catSatiety>0){
-        weight = weight - 5;
+
+    public boolean pee() {
+        if (catSatiety > 0) {
+            weight = weight - 5;
             System.out.println("Hozyin, davai ubirai");
-        return true;}
-        else {
+            return true;
+        } else {
             System.out.println("Сначала покорми");
             return false;
-        }   }
-    public static int getCount ()
-    {
+        }
+    }
+
+    public static int getCount() {
         return count;
     }
 
-    public void liveAnotherDay(){
-        for (int i = 1; i<25; i++){
-            int r = (int) (Math.random()*5);
-            switch(r)
-            {
-                case 0: eat(10.0);
-                break;
-                case 1: if(catSatiety<=0){
-                    System.out.println("Сначала покорми");
+    public void liveAnotherDay() {
+        for (int i = 1; i < 25; i++) {
+            int r = (int) (Math.random() * 5);
+            switch (r) {
+                case 0:
                     eat(10.0);
-                }
-                else {drink(10.0);}
-                break;
-                case 2: if(catSatiety<=0){
-                    System.out.println("Сначала покорми");
-                    eat(10.0);
-                }
-                else {pee();}
-                break;
-                case 3: if(catSatiety<=0){
-                    System.out.println("Сначала покорми");
-                    eat(10.0);
-                }
-                else {play();}
-                break;
-                case 4: if(catSatiety<=0){
-                    System.out.println("Сначала покорми");
-                    eat(10.0);
-                }
-                else {meow();}
-                break;
+                    break;
+                case 1:
+                    if (catSatiety <= 0) {
+                        System.out.println("Сначала покорми");
+                        eat(10.0);
+                    } else {
+                        drink(10.0);
+                    }
+                    break;
+                case 2:
+                    if (catSatiety <= 0) {
+                        System.out.println("Сначала покорми");
+                        eat(10.0);
+                    } else {
+                        pee();
+                    }
+                    break;
+                case 3:
+                    if (catSatiety <= 0) {
+                        System.out.println("Сначала покорми");
+                        eat(10.0);
+                    } else {
+                        play();
+                    }
+                    break;
+                case 4:
+                    if (catSatiety <= 0) {
+                        System.out.println("Сначала покорми");
+                        eat(10.0);
+                    } else {
+                        meow();
+                    }
+                    break;
             }
 
         }
